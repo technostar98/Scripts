d7bc719f5cee669ad9a7bcbab84a642faac7b8c4
Techno <bubdude@rochester.rr.com>
2016-04-29 19:54:14 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-screen -dmS mc java -server -Xms512m -Xmx8192M -XX:PermSize=1024m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:Parallel$
+screen -dmS mc java -server -Xms512m -Xmx8192M -XX:PermSize=1024m -d64 -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:+CMSClassUnloadingEnabled -XX:ParallelGCThreads=2 -XX:MinHeapFreeRatio=5 -XX:MaxHeapFreeRatio=10 -jar forge-1.7.10-10.13.4.1614-1.7.10-universal.jar nogui
